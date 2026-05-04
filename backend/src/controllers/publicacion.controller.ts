78df5c7c9407d6109ee8b24bf84f789521ee05e0
Miguel Alejandro Madrigal Ramírez <143459684+MigaleMR@users.noreply.github.com>
2026-05-04 12:05:03 -0600

    Update publicacion.controller.ts

--- backend/src/controllers/publicacion.controller.ts
+++ backend/src/controllers/publicacion.controller.ts
@@ -595,7 +595,7 @@ export const updatePublicacion = async (req: Request, res: Response): Promise<vo
       return;
     }
 
-    res.status(200).json(publicacionActualizada);
+    res.status(200).json(publicacion);
   } catch (error) {
     const err = error as Error;
     res.status(500).json({ message: err.message });
